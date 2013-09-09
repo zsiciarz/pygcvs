9e279383f9e0944394cee7a19c6ba16984154165
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-09 22:10:28 +0200

    Initial tests for parsing magnitude.

--- tests/parser.py
+++ tests/parser.py
@@ -6,5 +6,12 @@ from pygcvs.parser import GcvsParser
 
 
 class GcvsParserTestCase(unittest.TestCase):
-    def test_dummy(self):
+    def test_parse_simple_magnitude(self):
         parser = GcvsParser()
+        magnitude = '  8.45   '
+        self.assertAlmostEqual(parser.parse_magnitude(magnitude), 8.45)
+
+    def test_parse_two_digit_magnitude(self):
+        parser = GcvsParser()
+        magnitude = ' 12.2    '
+        self.assertAlmostEqual(parser.parse_magnitude(magnitude), 12.2)
