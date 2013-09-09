93798f4cd00d7b607190b38802c48c46d3bced94
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-09 22:01:58 +0200

    Dummy test for dummy class.

---
+++ tests/parser.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+import unittest
+
+from pygcvs.parser import GcvsParser
+
+
+class GcvsParserTestCase(unittest.TestCase):
+    def test_dummy(self):
+        parser = GcvsParser()
