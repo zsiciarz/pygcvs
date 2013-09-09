4cdb229cc15b368b1f75f8db280ffef5f4ed398e
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-09 23:40:46 +0200

    Added visualisation of magnitude differences.

---
+++ examples/plot_magnitudes.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals, print_function
+
+"""
+Visualisation of maximum/minimum magnitude for GCVS stars.
+"""
+
+import sys
+
+import matplotlib.pyplot as plot
+
+from pygcvs import read_gcvs
+
+
+if __name__ == '__main__':
+    try:
+        gcvs_file = sys.argv[1]
+    except IndexError:
+        print('Usage: python plot_magnitudes.py <path to iii.dat>')
+    else:
+        min_magnitudes = []
+        max_magnitudes = []
+        with open(gcvs_file, 'rb') as fp:
+            for star in read_gcvs(fp):
+                if star['min_magnitude'] and star['max_magnitude']:
+                    min_magnitudes.append(star['min_magnitude'])
+                    max_magnitudes.append(star['max_magnitude'])
+        plot.title('GCVS variable star magnitudes')
+        plot.plot(min_magnitudes, max_magnitudes, 'ro')
+        plot.xlabel('Min magnitude')
+        plot.ylabel('Max magnitude')
+        # invert axes because brightest stars have lowest magnitude value
+        plot.gca().invert_xaxis()
+        plot.gca().invert_yaxis()
+        plot.show()
