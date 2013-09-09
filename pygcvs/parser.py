8d6c16007b812380c7eb4e5df3a76ade48194da6
Zbigniew Siciarz <antyqjon@gmail.com>
2013-09-09 22:20:23 +0200

    Fleshing out the parser.

--- pygcvs/parser.py
+++ pygcvs/parser.py
@@ -7,7 +7,34 @@ logger = logging.getLogger(__name__)
 
 
 class GcvsParser(object):
+    """
+    A parser for GCVS data format.
+    """
+
+    def __init__(self, fp):
+        """
+        Creates the parser and feeds it a file-like object.
+        """
+        self.reader = csv.reader(fp, delimiter=str('|'))
+        # skip two initial lines
+        next(self.reader)
+        next(self.reader)
+
+    def __iter__(self):
+        for row in self.reader:
+            yield self.row_to_dict(row)
+
+    def row_to_dict(self, row):
+        """
+        TODO
+        """
+        return {}
+
     def parse_magnitude(self, magnitude_str):
+        """
+        Converts magnitude field to a float value, or ``None`` if GCVS does
+        not list the magnitude.
+        """
         mag_symbol = magnitude_str[0].strip()
         magnitude = magnitude_str[1:6].strip()
         return float(magnitude) if magnitude else None
