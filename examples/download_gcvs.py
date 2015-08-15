9af06f45710520ddde60c0d1031906d7a017c7ea
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-08-15 21:36:00 +0200

    Fix iterator in the download_gcvs example.

--- examples/download_gcvs.py
+++ examples/download_gcvs.py
@@ -10,6 +10,6 @@ from pygcvs.parser import GcvsParser
 if __name__ == '__main__':
     url = 'http://www.sai.msu.su/gcvs/gcvs/iii/iii.dat'
     response = requests.get(url, stream=True)
-    parser = GcvsParser(response.iter_lines())
+    parser = GcvsParser(response.iter_lines(decode_unicode=True))
     for star in parser:
         print(star)
