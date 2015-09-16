0fde4f6915af7507a698ee1bc8150a4e6e80311f
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-09-16 07:57:32 +0200

    Python 3.5 support.

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Utilities'
     ],
